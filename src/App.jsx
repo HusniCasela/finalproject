26441315fb97d15a9c957e7cb2bf191b8aa7f74e
kelascoding <133162689+wijoyoCell@users.noreply.github.com>
2023-11-25 16:08:59 +0700

    Hapus App.jsx yang lama dan Update App.jsx yang baru

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from '../src/pages/Login'
 import Register from '../src/pages/Register'
-function App() {
+import Reset from '../src/pages/Reset'
+function App(){
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path='/Login' element={<Login />}></Route>
           <Route path='/Register' element={<Register />}></Route>
+        <Route path='/Reset' element={<Reset/>}></Route>
         </Routes>
-      </BrowserRouter>
+     </BrowserRouter>
     </>
   )
 }
-
 export default App
