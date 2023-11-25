9ae06c0b326155e4c5e931ee8357733e295cceb8
kelascoding <133162689+wijoyoCell@users.noreply.github.com>
2023-11-25 16:23:14 +0700

    Hapus ResetPassword.jsx dan di update menjadi Reset.jsx yang baru

---
+++ src/pages/Reset.jsx
@@ -0,0 +1,10 @@
+import ResetPassword from'../components/ResetPassword/ResetPassword'
+const Reset= () => {
+    return(
+        <section className="">
+        <ResetPassword/>
+        <div class="bg-DARKBLUE05 w-3/5 h-full transform translate-x-full fixed top-0"></div>
+        </section>
+    )
+}
+export default Reset
