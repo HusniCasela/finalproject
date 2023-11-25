3eb193f4bdd89179956900a8d9acd91bc6eabfef
kelascoding <133162689+wijoyoCell@users.noreply.github.com>
2023-11-25 16:18:02 +0700

    Hapus Button.jsx lama dan Update Button.jsx yang baru

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
-const Button = ({ title}) => {
+const Button=({title}) => {
     return (
-        <div className="py-3 px-6 bg-DARKBLUE05 text-white text-center rounded-2xl mt-2 text-sm">
-            {title}
-        </div>
+ <button className="w-50 pt-3 pr-40 pb-3 pl-40 bg-DARKBLUE05 text-white rounded-xl">
+ {title}
+ </button>
     )
 }
 
