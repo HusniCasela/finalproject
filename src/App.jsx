9eca658b5a3ca614b680cfdc9f17a2c81633193d
husni47 <husnithecitizen@gmail.com>
2023-11-26 19:08:36 +0700

    buat page component ResetTautan.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from '../src/pages/Login'
-import Register from '../src/pages/Register'
-import Reset from '../src/pages/Reset'
-function App(){
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../src/pages/Login";
+import Register from "../src/pages/Register";
+import Reset from "../src/pages/Reset";
+import ResetTautan from "./pages/ResetTautan";
+function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/Login' element={<Login />}></Route>
-          <Route path='/Register' element={<Register />}></Route>
-        <Route path='/Reset' element={<Reset/>}></Route>
+          <Route path="/Login" element={<Login />}></Route>
+          <Route path="/Register" element={<Register />}></Route>
+          <Route path="/Reset" element={<Reset />}></Route>
+          <Route path="/ResetTautan" element={<ResetTautan />}></Route>
         </Routes>
-     </BrowserRouter>
+      </BrowserRouter>
     </>
-  )
+  );
 }
-export default App
+export default App;
---
+++ src/pages/ResetTautan.jsx
@@ -0,0 +1,10 @@
+import Tautan from "../components/ResetPassword/Tautan";
+const ResetTautan = () => {
+  return (
+    <section className="">
+      <Tautan />
+      <div class="bg-DARKBLUE05 w-3/5 h-full transform translate-x-full fixed top-0"></div>
+    </section>
+  );
+};
+export default ResetTautan;
