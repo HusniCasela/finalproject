7952c016fddd9131714cf09a4eb8a1e13333fea2
husni47 <husnithecitizen@gmail.com>
2023-11-28 13:45:07 +0700

    ngecomment button temenku

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,18 @@
 /* eslint-disable react/prop-types */
-const Button=({title,onClick}) => {
-    return (
-  <button class="w-THREESEVEN pt-2.5 pb-2.5 pl-3 rounded-xl border-2 bg-DARKBLUE05 text-white text-sm" title={title} onClick={onClick}>
-  {title}
-  </button>
-    )
-}
-export default Button
+const Button = ({ title, onClick }) => {
+  return (
+    <button
+      class="w-THREESEVEN pt-2.5 pb-2.5 pl-3 rounded-xl border-2 bg-DARKBLUE05 text-white text-sm"
+      title={title}
+      onClick={onClick}
+    >
+      {title}
+    </button>
+
+    // pake button temenku
+    // <div className="py-3 px-6 bg-DARKBLUE05 text-white text-center rounded-2xl mt-2 text-sm">
+    //   {title}
+    // </div>
+  );
+};
+export default Button;
