1b9566139f2e6b88215fc7d9c2ac0fda807bc39b
husni47 <husnithecitizen@gmail.com>
2023-11-28 17:00:38 +0700

    belum fix,mau di fix kan sama husni

--- src/pages/ResetTautan.jsx
+++ src/pages/ResetTautan.jsx
@@ -1,9 +1,17 @@
 import Tautan from "../components/ResetPassword/Tautan";
 const ResetTautan = () => {
   return (
-    <section className="">
-      <Tautan />
-      <div class="bg-DARKBLUE05 w-3/5 h-full transform translate-x-full fixed top-0"></div>
+    // <section className="">
+    //   <Tautan />
+    //   <div class="bg-DARKBLUE05 w-3/5 h-full transform translate-x-full fixed top-0"></div>
+    // </section>
+    <section className="mx-auto">
+      <div className="grid grid-cols-12 ">
+        <div className="col-span-7 my-auto  px-40">
+          <Tautan />
+        </div>
+        <div className="col-span-5 bg-DARKBLUE05 h-screen"></div>
+      </div>
     </section>
   );
 };
