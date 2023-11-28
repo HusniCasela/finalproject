46c2f3dcdb9dd6b3c1802c126a6eff346ab18156
husni47 <husnithecitizen@gmail.com>
2023-11-28 16:49:25 +0700

    update button pake punya temenku

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,18 +1,21 @@
 /* eslint-disable react/prop-types */
 const Button = ({ title, onClick }) => {
   return (
-    <button
-      class="w-THREESEVEN pt-2.5 pb-2.5 pl-3 rounded-xl border-2 bg-DARKBLUE05 text-white text-sm"
-      title={title}
+    // <button
+    //   class="w-THREESEVEN pt-2.5 pb-2.5 pl-3 rounded-xl border-2 bg-DARKBLUE05 text-white text-sm"
+    //   title={title}
+    //   onClick={onClick}
+    // >
+    //   {title}
+    // </button>
+
+    // pake button temenku
+    <div
+      className="py-3 px-6 bg-DARKBLUE05 text-white text-center rounded-2xl mt-2 text-sm"
       onClick={onClick}
     >
       {title}
-    </button>
-
-    // pake button temenku
-    // <div className="py-3 px-6 bg-DARKBLUE05 text-white text-center rounded-2xl mt-2 text-sm">
-    //   {title}
-    // </div>
+    </div>
   );
 };
 export default Button;
