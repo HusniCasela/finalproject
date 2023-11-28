9bf046f500e061dc5b70af66c693147050b72757
husni47 <husnithecitizen@gmail.com>
2023-11-29 03:36:06 +0700

    update mt-30 pada div agar responsive dan mt-8 pada alert agar responsive

--- src/components/ResetPassword/Tautan.jsx
+++ src/components/ResetPassword/Tautan.jsx
@@ -1,50 +1,53 @@
 import Button from "../Button/Button";
 import { useNavigate } from "react-router-dom";
-import React,{useRef,useState} from "react"
+import React, { useRef, useState } from "react";
 import TitleReset from "../InputPassword/TitleReset";
-import Allert from "../Allert/Allert"
+import Allert from "../Allert/Allert";
 const Tautan = () => {
-const [warningMailRed,setWarningMailRed] = useState("hidden")
-const [warningMailGreen,setWarningMailGreen] = useState("hidden")
- const dataMail = "contoh@gmail.com"
- const ipMail = useRef(null)
-  const sendMail =()=>{
-if(ipMail.current.value === dataMail){
- setWarningMailGreen("block")
- setWarningMailRed("hidden")
-}else{
- setWarningMailRed("block")
- setWarningMailGreen("hidden")
-}
-}
+  const [warningMailRed, setWarningMailRed] = useState("hidden");
+  const [warningMailGreen, setWarningMailGreen] = useState("hidden");
+  const dataMail = "contoh@gmail.com";
+  const ipMail = useRef(null);
+  const sendMail = () => {
+    if (ipMail.current.value === dataMail) {
+      setWarningMailGreen("block");
+      setWarningMailRed("hidden");
+    } else {
+      setWarningMailRed("block");
+      setWarningMailGreen("hidden");
+    }
+  };
   return (
     <section>
-      <div className="mt-60 ml-20">
-    <TitleReset titleMessage={"Reset Password"}/>
+      <div className="mt-30 ml-20">
+        <TitleReset titleMessage={"Reset Password"} />
         <div className="pt-8 pr-0 pb-0 pl-0">
-            <label className="text-sm/[6px] font-normal pb-1.5">
-              Masukan Email Untuk Menerima Tautan 
-            </label>
-            </div>
-            <div>
-              <input
-                type="email"
-                className="w-THREESEVEN pt-2.5 pb-2.5 pl-3 pr-14 rounded-xl border-2 text-sm" ref={ipMail}
-                required
-              />
-            </div>
-          <div className="pt-8 pr-0 pb-8 pl-0">
- <Button title={"Kirim"} onClick={sendMail}></Button>
-          </div>
-         
-<div className={`${warningMailRed} ml-LEFTWR mt-14`}>
- <Allert type="warning" message={"Email tidak terdaftar"}/>
- </div>
-<div className={`${warningMailGreen} ml-LEFTWG mt-14`}>
-<Allert type="success" message={"buka email untuk melihat tautan reset"}/>
- </div>
- </div>
-     
+          <label className="text-sm/[6px] font-normal pb-1.5">
+            Masukan Email Untuk Menerima Tautan
+          </label>
+        </div>
+        <div>
+          <input
+            type="email"
+            className="border-2 border-neutral-200 text-sm rounded-2xl px-4 py-3 w-full"
+            ref={ipMail}
+            required
+          />
+        </div>
+        <div className="pt-8 pr-0 pb-8 pl-0">
+          <Button title={"Kirim"} onClick={sendMail}></Button>
+        </div>
+
+        <div className={`${warningMailRed} ml-LEFTWR mt-8 `}>
+          <Allert type="warning" message={"Email tidak terdaftar"} />
+        </div>
+        <div className={`${warningMailGreen} ml-LEFTWG mt-8 `}>
+          <Allert
+            type="success"
+            message={"buka email untuk melihat tautan reset"}
+          />
+        </div>
+      </div>
     </section>
   );
 };
