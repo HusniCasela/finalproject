b20545498c5749f6778eb6904df491c2202dd808
husni47 <husnithecitizen@gmail.com>
2023-11-28 16:59:02 +0700

    ubah ResetPassword,Reset.jsx sesuai dengan temenku

--- src/components/ResetPassword/ResetPassword.jsx
+++ src/components/ResetPassword/ResetPassword.jsx
@@ -1,14 +1,14 @@
-import { useNavigate } from "react-router-dom" 
-import InputTwo from "../InputPassword/InputPasswordTwo"
-import TitleReset from "../InputPassword/TitleReset"
-const ResetPassword = ()=>{
- return(
-<section>
- <div className="mt-60 ml-24"> 
- <TitleReset titleMessage={"Reset Password"}/>
-<InputTwo/>
- </div>
-</section>
-)
-}
-export default ResetPassword
+import { useNavigate } from "react-router-dom";
+import InputTwo from "../InputPassword/InputPasswordTwo";
+import TitleReset from "../InputPassword/TitleReset";
+const ResetPassword = () => {
+  return (
+    <section>
+      <div className="">
+        <TitleReset titleMessage={"Reset Password"} />
+        <InputTwo />
+      </div>
+    </section>
+  );
+};
+export default ResetPassword;
--- src/pages/Reset.jsx
+++ src/pages/Reset.jsx
@@ -1,10 +1,18 @@
-import ResetPassword from'../components/ResetPassword/ResetPassword'
-const Reset= () => {
-    return(
-        <section className="">
-        <ResetPassword/>
-        <div class="bg-DARKBLUE05 w-3/5 h-full transform translate-x-full fixed top-0"></div>
-        </section>
-    )
-}
-export default Reset
+import ResetPassword from "../components/ResetPassword/ResetPassword";
+const Reset = () => {
+  return (
+    // <section className="">
+    // <ResetPassword/>
+    // <div class="bg-DARKBLUE05 w-3/5 h-full transform translate-x-full fixed top-0"></div>
+    // </section>
+    <section className="mx-auto">
+      <div className="grid grid-cols-12 ">
+        <div className="col-span-7 my-auto  px-40">
+          <ResetPassword />
+        </div>
+        <div className="col-span-5 bg-DARKBLUE05 h-screen"></div>
+      </div>
+    </section>
+  );
+};
+export default Reset;
