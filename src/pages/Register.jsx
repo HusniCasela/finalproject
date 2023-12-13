729b6ae7376f1902fb36d6c842277c90b84c495c
kelascoding <133162689+wijoyoCell@users.noreply.github.com>
2023-12-13 19:58:19 +0700

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
+/*update*/
 import RegisterForm from "../components/LoginAndRegister/RegisterForm";
-
+import Logo from "../assets/logo.jpg";
 const Register = () => {
   return (
     <section className="mx-auto">
@@ -7,7 +8,11 @@ const Register = () => {
         <div className="col-span-7 my-auto  px-40">
           <RegisterForm />
         </div>
-        <div className="col-span-5 bg-DARKBLUE05 h-screen"></div>
+        <div className="col-span-5 bg-DARKBLUE05 h-screen"> 
+        <div className="flex justify-center mt-80">
+        <img src={Logo} className="w-80"/>
+        </div>
+        </div>
       </div>
     </section>
   );
