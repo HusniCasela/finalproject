31087280fb7f0071b59076720f93458e92ffc2ff
husni47 <husnithecitizen@gmail.com>
2023-12-14 01:33:52 +0700

    update logo.jpg

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,10 +8,10 @@ const Register = () => {
         <div className="col-span-7 my-auto  px-40">
           <RegisterForm />
         </div>
-        <div className="col-span-5 bg-DARKBLUE05 h-screen"> 
-        <div className="flex justify-center mt-80">
-        <img src={Logo} className="w-80"/>
-        </div>
+        <div className="col-span-5 bg-DARKBLUE05 h-screen">
+          <div className="flex justify-center mt-80">
+            <img src={Logo} className="w-80" />
+          </div>
         </div>
       </div>
     </section>
