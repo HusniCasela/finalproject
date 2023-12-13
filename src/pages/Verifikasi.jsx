802e844b248084b079bd3d52221966c20b60870d
kelascoding <133162689+wijoyoCell@users.noreply.github.com>
2023-12-13 19:57:43 +0700

    Update Verifikasi.jsx

--- src/pages/Verifikasi.jsx
+++ src/pages/Verifikasi.jsx
@@ -1,14 +1,21 @@
+/*update*/
 import VerifikasiOtp from'../components/ResetPassword/VerifikasiOtp'
-const Verifikasi = () => {
+import Logo from "../assets/logo.jpg";
+ const Verifikasi = () => {
     return(
     <section className="mx-auto">
       <div className="grid grid-cols-12 ">
         <div className="col-span-7 my-auto  px-40">
       <VerifikasiOtp />
         </div>
-        <div className="col-span-5 bg-DARKBLUE05 h-screen"></div>
+        <div className="col-span-5 bg-DARKBLUE05 h-screen">
+        <div className="flex justify-center mt-80">
+        <img src={Logo} className="w-80"/>
+        </div>
+        </div>
       </div>
     </section>
     )
 }
 export default Verifikasi
+
