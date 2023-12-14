7d2d7cf32ae077b3db31a5a0b881d6835eba9d06
kelascoding <133162689+wijoyoCell@users.noreply.github.com>
2023-12-15 00:36:05 +0700

    Update InputPasswordTwo.jsx

--- src/components/InputPassword/InputPasswordTwo.jsx
+++ src/components/InputPassword/InputPasswordTwo.jsx
@@ -3,6 +3,8 @@ import Eye from "../../assets/eye.svg";
 import EyeSlash from "../../assets/eyeslash.svg";
 import Button from "../Button/Button";
 import Allert from "../Allert/Allert";
+import axios from "axios"; 
+import { SERVER_URL } from "../../lib/constant";
 const InputTwo = () => {
   let inputShowHide = useRef(null);
   let inputShowHideO = useRef(null);
@@ -20,7 +22,9 @@ const InputTwo = () => {
   const [eyeShOne, setEyeShOne] = useState("hidden");
   const [eyeHdTwo, setEyeHdTwo] = useState("");
   const [eyeShTwo, setEyeShTwo] = useState("hidden");
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+ 
   const show = () => {
     inputShowHide.current.type = "text";
     setEyeHdTwo("hidden");
@@ -41,37 +45,59 @@ const InputTwo = () => {
     setEyeHdOne("hidden");
     setEyeShOne("block");
   };
-  const butt = () => {
-    if (
-      inputShowHide.current.value.length >= 8 &&
+  
+  const butt = async () => {
+    try {
+      console.log({ email, password });
+      const response = await axios.put(
+        `${SERVER_URL}/auth/reset-password`,
+        JSON.stringify({ email, password }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status === 200 &&      inputShowHide.current.value.length >= 8 &&
       inputShowHideO.current.value.length >= 8 &&
-      inputShowHide.current.value === inputShowHideO.current.value
-    ) {
+      inputShowHide.current.value === inputShowHideO.current.value) {
+        console.log("reset password sukses!");
       setInputOne("border-2");
       setInputTwo("border-2");
       setSuccessGreen("block");
       setWarningRed("hidden");
       setWarningRedTwo("hidden");
-    } else if (inputShowHide.current.value !== inputShowHideO.current.value) {
-      setInputOne("border-WARNING");
+      } else if(inputShowHide.current.value !== inputShowHideO.current.value){
+        console.log("Reset password gagal. Kode status:", response.status);
+         setInputOne("border-WARNING");
       setInputTwo("border-WARNING");
       setWarningRedTwo("block");
       setSuccessGreen("hidden");
       setWarningRed("hidden");
-    } else {
-      setInputOne("border-WARNING");
+      }else{
+       setInputOne("border-WARNING");
       setInputTwo("border-WARNING");
       setWarningRed("block");
       setSuccessGreen("hidden");
       setWarningRedTwo("hidden");
+      }
+    } catch (error) {
+      console.error("Error:", error.message);
     }
+    
+    
   };
-
+  /*
+  const butt = () => {
+    
+  };
+*/
   return (
     <div>
       <div className="mt-8">
         <label className="text-xs font-normal pb-1.5">
-          Masukan Password Baru
+          Masukan Email Anda 
         </label>
         <div className="relative">
           <img
@@ -89,16 +115,17 @@ const InputTwo = () => {
             ref={eyeOs}
           />
           <input
-            type="password"
+            type="email"
             ref={inputShowHideO}
             className={` border-2 border-neutral-200 text-sm rounded-2xl px-4 py-3 w-full ${inputOne}`}
-            required
+           value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
       </div>
       <div className="mt-6">
         <label className="text-xs font-normal pb-1.5">
-          Ulangi Password Baru
+          Masukan Password Baru
         </label>
         <div className="relative">
           <img
@@ -118,7 +145,9 @@ const InputTwo = () => {
           <input
             type="password"
             ref={inputShowHide}
-            className={` border-2 border-neutral-200 text-sm rounded-2xl px-4 py-3 w-full ${inputTwo}`}
+            className={` border-2 border-neutral-200 text-sm rounded-2xl px-4 py-3 w-full ${inputTwo}`} 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             required
           />
         </div>
@@ -137,7 +166,7 @@ const InputTwo = () => {
         </div>
 
         <div className={`${successGreen} ml-LEFT mt-8 `}>
-          <Allert type="success" message={"Reset password berhasil"} />
+          <Allert type="success" message={"Reset tautan terkirim"} />
         </div>
       </div>
     </div>
